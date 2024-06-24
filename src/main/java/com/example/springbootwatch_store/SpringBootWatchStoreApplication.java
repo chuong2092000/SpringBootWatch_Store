e5946938cc36b1f481f49da3d530e02f03c93adb
chuong2092000 <122428010+chuong2092000@users.noreply.github.com>
2024-06-24 12:04:38 +0700

    first commit

---
+++ src/main/java/com/example/springbootwatch_store/SpringBootWatchStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwatch_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWatchStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWatchStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwatch_store/SpringBootWatchStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwatch_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWatchStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
